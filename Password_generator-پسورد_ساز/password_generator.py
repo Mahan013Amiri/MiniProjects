b7ffd8bd4c958ab3e4241afdfefd68cfcde2f2c1
Mahan <mahanst3@gmail.com>
2024-08-06 23:02:12 +0330

    created password_generator.py

--- Password_generator-پسورد_ساز/password_generator.py
+++ Password_generator-پسورد_ساز/password_generator.py
@@ -1,1 +1,11 @@
-print("hi")
+import random, string
+
+lower_letters = string.ascii_lowercase
+upper_letters = string.ascii_uppercase
+numbers = "0123456789"
+characters = "!@#$%^&*()_+-="
+all = lower_letters + upper_letters + numbers + characters
+password = ""
+for i in range(8):
+    password += random.choice(all)
+print(password)
